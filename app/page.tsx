2b5f0ad224b2123fe3455c91e261d0a39bd6acae
kamal <dhimank079@gmail.com>
2023-10-29 18:24:32 +0530

    add coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function Home () {
   return (
-    <main></main>
+    <main className='h-screen w-screen flex items-center justify-center'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-semibold'>Clouser</h1>
+        <h3>Coming Soon</h3>
+      </div>
+    </main>
   )
 }
