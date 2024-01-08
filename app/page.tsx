59992035b8183f25e74b9ee27c59b105fb186d8f
kamal <dhimank079@gmail.com>
2024-01-08 15:46:06 +0530

    fix name

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home () {
   return (
     <main className='h-screen w-screen flex items-center justify-center'>
       <div className='text-center'>
-        <h1 className='text-4xl font-semibold'>Clouser</h1>
+        <h1 className='text-4xl font-semibold'>Clousor</h1>
         <h3>Coming Soon</h3>
       </div>
     </main>
